3c34d128e9dc2d52d191deb29082ef4a933b97c5
moust4ki <moust4ki@epitech.eu>
2026-02-08 18:17:18 +0100

    add files

--- ble_jammer/info.cpp
+++ ble_jammer/info.cpp
@@ -5,12 +5,12 @@ void afficherInfos() {
   ecranOLED.setTextSize(1);
   ecranOLED.setTextColor(SSD1306_WHITE);
   ecranOLED.setCursor(0, 16);
-  ecranOLED.println(F("Version: 1.0.9"));
-  ecranOLED.println(F("User:    Moust4ki"));
-  ecranOLED.println(F("HW:      ESP32"));
+  ecranOLED.println(F("Version : 1.0.9"));
+  ecranOLED.println(F("User    : Moust4ki"));
+  ecranOLED.println(F("HW      : ESP32"));
   ecranOLED.setCursor(0, 45);
-  ecranOLED.println(F("Status:  Ready"));
-  ecranOLED.print(F("Uptime:    ")); 
+  ecranOLED.println(F("Status  : Ready"));
+  ecranOLED.print(F("Uptime  : ")); 
   ecranOLED.print(millis() / 1000);
   ecranOLED.println(F("s"));
 }
